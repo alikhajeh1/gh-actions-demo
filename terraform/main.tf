c665f4857a31789ea9ee14899cfd69146e38dc84
Ali Khajeh-Hosseini <ali@infracost.io>
2022-08-03 11:02:35 -0700

    Add more IOPs

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,8 +17,8 @@ resource "aws_instance" "web_app" {
   ebs_block_device {
     device_name = "my_data"
     volume_type = "io1"
-    volume_size = 3000
-    iops        = 1000                      # <<<<< Try changing this to 10000 to compare costs
+    volume_size = 5000
+    iops        = 3000                      # <<<<< Try changing this to 10000 to compare costs
   }
 }
 
